f522155dfadb22e1bba5f0a10421c8756a366c89
Rheddes <rens.h@me.com>
2023-12-20 08:07:10 +0100

    Init day11

---
+++ day11/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn part1(input: &str) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod test_day11 {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = include_str!("../resources/example.txt");
+        assert_eq!(part1(input), 374);
+    }
+}
---
+++ day11/src/main.rs
@@ -0,0 +1,6 @@
+use day11::part1;
+
+fn main() {
+    let input = include_str!("../resource/input.txt");
+    println!("Part 1: {}", part1(input));
+}
